879b5cb815c9423073373eb869087823a84090bd
Kritish Dhaubanjar <kritish.dhaubanjar@gmail.com>
2024-10-07 23:07:06 +0545

    feat(app): add delete, create and update unit

--- app/src/services/unit.service.js
+++ app/src/services/unit.service.js
@@ -5,3 +5,21 @@ export async function getUnits({ page, limit, query }) {
 
   return data;
 }
+
+export async function deleteUnit({ id }) {
+  const data = await axios.delete(`/units/${id}`);
+
+  return data;
+}
+
+export async function createUnit({ name }) {
+  const data = await axios.post('/units', { name });
+
+  return data;
+}
+
+export async function updateUnit({ name, id }) {
+  const data = await axios.put(`/units/${id}`, { name, id });
+
+  return data;
+}
