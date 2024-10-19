019c0b7bd98ddcad616352f23355280ebe46f4c2
Kritish Dhaubanjar <kritish.dhaubanjar@gmail.com>
2024-10-19 14:19:00 +0545

    feat(api): add id to units

--- api/database/migrations/2024_10_05_142027_create_units_table.php
+++ api/database/migrations/2024_10_05_142027_create_units_table.php
@@ -18,23 +18,23 @@ return new class extends Migration
             $table->timestamps();
         });
 
-        DB::table('units')->insert(['name' => 'Pcs']);
-        DB::table('units')->insert(['name' => 'Pcs/n']);
-        DB::table('units')->insert(['name' => 'Pkt']);
-        DB::table('units')->insert(['name' => 'Pkt/n']);
-        DB::table('units')->insert(['name' => 'Sqr']);
-        DB::table('units')->insert(['name' => 'Sqr/n']);
-        DB::table('units')->insert(['name' => 'Set']);
-        DB::table('units')->insert(['name' => 'Set/n']);
-        DB::table('units')->insert(['name' => 'Mtr']);
-        DB::table('units')->insert(['name' => 'Mtr/n']);
-        DB::table('units')->insert(['name' => 'Bot./n']);
-        DB::table('units')->insert(['name' => 'Kg/n']);
-        DB::table('units')->insert(['name' => 'Bag/n']);
-        DB::table('units')->insert(['name' => 'Can/n']);
-        DB::table('units')->insert(['name' => 'Ml/T']);
-        DB::table('units')->insert(['name' => 'Side To']);
-        DB::table('units')->insert(['name' => 'Jar/n']);
+        DB::table('units')->insert(['id' => 1, 'name' => 'Pcs']);
+        DB::table('units')->insert(['id' => 2, 'name' => 'Pcs/n']);
+        DB::table('units')->insert(['id' => 3, 'name' => 'Pkt']);
+        DB::table('units')->insert(['id' => 4, 'name' => 'Pkt/n']);
+        DB::table('units')->insert(['id' => 5, 'name' => 'Sqr']);
+        DB::table('units')->insert(['id' => 6, 'name' => 'Sqr/n']);
+        DB::table('units')->insert(['id' => 7, 'name' => 'Set']);
+        DB::table('units')->insert(['id' => 8, 'name' => 'Set/n']);
+        DB::table('units')->insert(['id' => 9, 'name' => 'Mtr']);
+        DB::table('units')->insert(['id' => 10, 'name' => 'Mtr/n']);
+        DB::table('units')->insert(['id' => 11, 'name' => 'Bot./n']);
+        DB::table('units')->insert(['id' => 12, 'name' => 'Kg/n']);
+        DB::table('units')->insert(['id' => 13, 'name' => 'Bag/n']);
+        DB::table('units')->insert(['id' => 14, 'name' => 'Can/n']);
+        DB::table('units')->insert(['id' => 15, 'name' => 'Ml/T']);
+        DB::table('units')->insert(['id' => 16, 'name' => 'Side To']);
+        DB::table('units')->insert(['id' => 17, 'name' => 'Jar/n']);
     }
 
     /**
