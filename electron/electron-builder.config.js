59b544d45467ebcf5f09eb93d1a1a4284b97a4e3
Kritish Dhaubanjar <kritish.dhaubanjar@gmail.com>
2024-12-02 00:32:00 +0545

    chore(electron): include .gitignore and .gitkeep

--- electron/electron-builder.config.js
+++ electron/electron-builder.config.js
@@ -2,6 +2,6 @@ module.exports = {
   appId: 'sales-trail',
   productName: 'Sales Trail',
   asar: false,
-  files: ['php-bin', 'api', 'assets', 'index.js', 'php.js'],
+  files: ['php-bin', 'api', 'assets', 'index.js', 'php.js', '**/.gitignore', '**/.gitkeep'],
   icon: './assets/icon.png'
 }
