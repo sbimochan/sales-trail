483bf5d84f61d7ca98f3b67c27cb1695b978b2a6
Kritish Dhaubanjar <kritish.dhaubanjar@gmail.com>
2024-12-02 00:40:54 +0545

    fix(electron): include api dir

--- electron/electron-builder.config.js
+++ electron/electron-builder.config.js
@@ -2,6 +2,6 @@ module.exports = {
   appId: 'sales-trail',
   productName: 'Sales Trail',
   asar: false,
-  files: ['php-bin', 'api', 'assets', 'index.js', 'php.js', '**/.gitignore', '**/.gitkeep'],
+  files: ['php-bin', 'assets', 'index.js', 'php.js', { from: "api", to: "api", filter: ["**/*"] }],
   icon: './assets/icon.png'
 }
