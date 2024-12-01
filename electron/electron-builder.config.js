93469c92f66dd0a02af4a826ad714666006fbe4e
Kritish Dhaubanjar <kritish.dhaubanjar@gmail.com>
2024-12-01 23:02:03 +0545

    fix(electron): fix missing php.js from build

--- electron/electron-builder.config.js
+++ electron/electron-builder.config.js
@@ -2,6 +2,6 @@ module.exports = {
   appId: 'sales-trail',
   productName: 'Sales Trail',
   asar: false,
-  files: ['php-bin', 'api', 'assets', 'index.js'],
+  files: ['php-bin', 'api', 'assets', 'index.js', 'php.js'],
   icon: './assets/icon.png'
 }
